48d05edda2071cfee8024ddbd520f3af1900f8a1
piomin <piotr.minkowski@gmail.com>
2023-02-02 09:50:36 +0100

    enable auto-update

--- src/test/java/pl/piomin/jasperreport/JasperApplicationTest.java
+++ src/test/java/pl/piomin/jasperreport/JasperApplicationTest.java
@@ -3,25 +3,33 @@ package pl.piomin.jasperreport;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Logger;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JasperApplicationTest {
 
-	protected Logger logger = Logger.getLogger(JasperApplicationTest.class.getName());
+	protected Logger logger = LoggerFactory.getLogger(JasperApplicationTest.class.getName());
 
-	TestRestTemplate template = new TestRestTemplate();
+	@Autowired
+	TestRestTemplate template;
 	
 	@Test
-	public void testGetReport() throws InterruptedException {
+	void testGetReport() throws InterruptedException {
 		List<HttpStatus> responses = new ArrayList<>();
 		Random r = new Random();
 		int i = 0;
+
+		long start = System.currentTimeMillis();
 		for (; i < 20; i++) {
 			new Thread(new Runnable() {
 				
@@ -29,7 +37,7 @@ public class JasperApplicationTest {
 				public void run() {
 					int age = r.nextInt(99);
 					long start = System.currentTimeMillis();
-					ResponseEntity<InputStreamResource> res = template.getForEntity("http://localhost:2222/pdf/fv/{age}", InputStreamResource.class, age);
+					ResponseEntity<InputStreamResource> res = template.getForEntity("/pdf/fv/{age}", InputStreamResource.class, age);
 					logger.info("Response (" +  (System.currentTimeMillis()-start) + "): " + res.getStatusCode());
 					responses.add(res.getStatusCode());
 					try {
@@ -43,9 +51,12 @@ public class JasperApplicationTest {
 		
 		while (responses.size() != i) {
 			Thread.sleep(500);
+			if (System.currentTimeMillis() - start > 10000)
+				break;
 		}
 		
-		logger.info("Test finished");
+		logger.info("Test finished: ok->{}, expected->{}", responses.size(), i);
+		Assertions.assertEquals(i, responses.size());
 	}
 		
 }
